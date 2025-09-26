56c9c1bf2a725c4919a5d2a3d539fba42a877f22
7embola-py <yahyamorchid64@gmail.com>
2025-09-26 20:55:23 +0100

    gitthings

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,14 @@
 
-variable1 = 10
-variable2 = 5
+number1 = 10
+number2 = 5
 #ADDITION:
-result = variable1 + variable2
-print(f"Addition of {variable1} and {variable2} is {result}")
+result = number1 + number2
+print(f"Addition of {number1} and {number2} is {result}")
 
 #SUBTRACTION:
-result = variable1 - variable2
-print(f"Subtraction of {variable1} and {variable2} is {result}")
+result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {result}")
 
 #MULTIPLICATION:
-result = variable1 * variable2
-print(f"Multiplication of {variable1} and {variable2} is {result}")
+result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {result}")
